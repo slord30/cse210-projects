c8f5c07b4ec977cb74e0f5d2f1bbfcd96bb6bfe5
slord30 <sarahlord79@gmail.com>
2024-09-19 18:06:23 -0500

    first commit with prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,24 @@
+/* Write a program that prompts the user for their first name. Then prompt them for their last name. Display the text back all on one line saying, "Your name is last-name, first-name, last-name".
+Example: What is your first name?
+What is your last name?
+Your name is Lord, Sarah Lord */
+
 using System;
 
-class Program
+class program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+
     }
 }
+
+
