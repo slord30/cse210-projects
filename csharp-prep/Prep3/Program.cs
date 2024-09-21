6514c2d74360c2957053c8f6861afc6f31e97531
slord30 <sarahlord79@gmail.com>
2024-09-21 11:05:24 -0500

    first commit Prep3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // generate a random number for the magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        // create global variable guess and set it equal to -1
+        int guess = -1;
+
+        // use a while loop to keep asking the user for a number until they guess the magic number
+        while (guess != magicNumber)
+        {
+            // Ask the user to guess a number
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
