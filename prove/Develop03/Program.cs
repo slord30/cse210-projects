ea575932c6f5acdc26fefad608913dd42959a163
slord30 <sarahlord79@gmail.com>
2024-10-04 18:27:04 -0500

    first commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop03 World!");
+
+    static void Main()
+    {      
+        Scripture scripture = new Scripture("3 Nephi ", 12, 16, "Therefore let your light so shine before this people, that they may see your good works and glorify your Father who is in heaven.");
+        Console.Clear();
+        scripture.Display();
+        Console.WriteLine();
+        Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                break;
+
+            if (!scripture.HideRandomWord())
+            {
+                Console.WriteLine("\nNice job! You memeorized the scripture!");
+                break;
+            }
+
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+        }
     }
 }
+
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public string GetDisplayText()
+    {
+        return _text;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+}
+
