0608df315e97d703992cb91aecd7c1e4365b2dfc
slord30 <sarahlord79@gmail.com>
2024-10-21 15:43:58 -0500

    first commit

---
+++ prepare/Learning06/Circle.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.NetworkInformation;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("purple", 4.5);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("green", 3.6, 2);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("red", 8);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+            double roundArea = Math.Round(area, 2);
+
+            Console.WriteLine($"The {color} shape has an area of {roundArea}.");
+        }
+        
+
     }
 }
